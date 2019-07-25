86e0bb0bf23a8aac8341bbf58adb6c86bbd9f2e8
TimmocEdoc <43539006+TimmocEdoc@users.noreply.github.com>
2019-07-25 15:05:52 +0700

    Add files via upload

---
+++ asg2/asg2/Cart.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+
+namespace asg2
+{
+    public class Cart
+    {
+        public int Id;
+        public string Customer;
+        public List<int> GrandTotal = new List<int>();
+        public List<string> ProductList = new List<string>();
+        public string City;
+        public string Country;
+        public int Total;
+        public string listElement;
+        public int price;
+        public Cart()
+        {
+            
+        }
+        public Cart(int id, string customer, string city, string country, int total)
+        {
+            Id = id;
+            Customer = customer;
+            City = city;
+            Country = country;
+            Total = total;
+        }
+        public void AddProduct()
+        {
+            if (ProductList.Count == 0)
+            {
+                Console.WriteLine("Add id:");
+                Id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Add customer:");
+                Customer = Console.ReadLine();
+                Console.WriteLine("Add city:");
+                City = Console.ReadLine();
+                Console.WriteLine("Add country:");
+                Country = Console.ReadLine();
+                Console.WriteLine("Add product:");
+                listElement = Console.ReadLine();
+                Console.WriteLine("Add price:");
+                price = Convert.ToInt32(Console.ReadLine());
+                foreach (string n in ProductList)
+                {
+                    if (listElement == n)
+                    {
+                        GrandTotal.Add(price);
+                        return;
+                    }
+                }
+                ProductList.Add(listElement);
+                GrandTotal.Add(price);
+            }
+            else
+            {
+                Console.WriteLine("Add product:");
+                listElement = Console.ReadLine();
+                Console.WriteLine("Add price:");
+                price = Convert.ToInt32(Console.ReadLine());
+                foreach (string n in ProductList)
+                {
+                    if (listElement == n)
+                    {
+                        GrandTotal.Add(price);
+                        return;
+                    }
+                }
+                ProductList.Add(listElement);
+                GrandTotal.Add(price);
+            }
+        }
+
+        public void RemoveProduct()
+        {
+            Console.WriteLine("Enter product name:");
+            String Product = Console.ReadLine();
+            int i=0;
+            foreach (string n in ProductList)
+            {
+                if (Product == n)
+                {
+                    ProductList.RemoveAt(i);
+                    GrandTotal.RemoveAt(i);
+                    Console.WriteLine("success!");
+                    return;
+                }
+                i++;
+            }
+            Console.WriteLine("There's no such things.");
+        }
+
+        public void ShowCart()
+        {
+            double grandTotal = 0;
+            foreach (int price in GrandTotal)
+            {
+                grandTotal = price + grandTotal;
+            }
+            if (City == "Ha Noi" || City == "HCM")
+            {
+                grandTotal = (grandTotal / 100 * 1) + grandTotal;
+            }
+            else if(Country == "Viet Nam")
+            {
+                grandTotal = (grandTotal / 100 * 2) + grandTotal;
+            }
+            else
+            {
+                grandTotal = (grandTotal / 100 * 5) + grandTotal;
+            }
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Id:"+Id);
+            Console.WriteLine("Customer:"+Customer);
+            Console.WriteLine("Item list:");
+            foreach (String n in ProductList)
+            {
+                Console.WriteLine("---Item: "+n);   
+            }
+            Console.WriteLine("City:"+City);
+            Console.WriteLine("Country:"+Country);
+            Console.WriteLine("Total:"+grandTotal);
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
---
+++ asg2/asg2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace asg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cart c = new Cart();
+            do
+            {
+              int choose;
+                Console.WriteLine("Choose:");
+                Console.WriteLine("1| Add product.");
+                Console.WriteLine("2| Remove product.");
+                Console.WriteLine("3| Show cart.");
+                Console.WriteLine("4| Exit.");
+                choose = Convert.ToInt32(Console.ReadLine());
+                switch (choose)
+                {
+                    case 1:
+                        c.AddProduct();
+                        Cart ca = new Cart(c.Id, c.Customer, c.City, c.Country, c.Total);
+                        break;
+                    case 2:
+                        c.RemoveProduct();
+                       break;
+                    case 3:
+                        c.ShowCart();
+                        break;
+                    case 4:
+                        System.Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Error.");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
